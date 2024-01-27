990e91cda4c183d83998728b98aa1a8df740712a
Attilioap <attilio.polito@mail.polimi.it>
2024-01-27 12:45:44 +0100

    itupdat

--- IT/Implementazione/codekatabattle/ckbapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CkbappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ckbapp'
--- IT/Implementazione/codekatabattle/ckbapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import educator_dash
-
-urlpatterns = [
-    path('educator_dashboard/', educator_dash),
-]
